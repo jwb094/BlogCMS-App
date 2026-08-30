aecee33a48db50d9265de2c595a23b25223c2d1d
Josh Bello <Jb230913@live.com>
2026-08-30 22:43:25 +0100

    add - new routes for public and admin side of application

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//Public - Frontend
+    Route::get('/', function () {
+            return view('frontend.index');
+        });
+
+        Route::get('/blogs', function () {
+            return view('frontend.blog');
+        });
+          Route::get('/post/{id}', function () {
+            return view('frontend.post');
+        });
+          Route::get('/post/{id}', function () {
+            return view('frontend.post');
+        });
+
+//Admin - Backend
+Route::prefix('admin')->group(function () {
+    Route::prefix('admin/posts/')->group(function () {
+        Route::get('post/add', function () {
+                return view('frontend.post');
+        });
+           Route::get('post/edit', function () {
+                return view('frontend.post');
+        });
+           Route::get('post/delete', function () {
+                return view('frontend.post');
+        });
+    });
+  
+
 });
