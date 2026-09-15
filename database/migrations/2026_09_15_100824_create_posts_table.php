fe1395ca83ad0dddc489767e03b7676331562f18
Josh Bello <Jb230913@live.com>
2026-09-15 16:34:13 +0100

    add - new file with schema created for insertion

---
+++ database/migrations/2026_09_15_100824_create_posts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('subheading');
+            $table->text('excerpt');
+            $table->enum('status', ['live', 'draft', 'scheduled']);
+            $table->enum('allow_comments', ['allow', 'forbidden']);
+            $table->string('featured_image');
+            $table->string('featured_image_caption');
+            $table->text('content');
+            $table->string('meta_title');
+            $table->string('meta_description');
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+            $table->foreignId('category_id')
+                ->constrained('categories')
+                ->cascadeOnDelete();
+            $table->timestamp('published_at');
+            $table->timestamp('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
