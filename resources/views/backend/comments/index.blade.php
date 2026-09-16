dc03c99e089e8ac6cd60f0ec048096b724e2d775
Josh Bello <Jb230913@live.com>
2026-09-16 16:43:12 +0100

    add - new components,views for frontend/backend/form

---
+++ resources/views/backend/comments/index.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+</div>
+@extends('layouts.backend-layout')
+@section('title','Comments')
+@section('content')
+
+@endsection
