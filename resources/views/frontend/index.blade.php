37a2ec1e5fac0118488ecee093c54a5b21755e61
Josh Bello <Jb230913@live.com>
2026-09-18 12:31:37 +0100

    update - add components to pages

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -7,9 +7,23 @@
     <!-- Content here -->
 
     <div class="container text-center">
+                    <h1>GGG</h1>
         <div class="row align-items-start">
             <div class="col-12 col-md-12">
-                <h1>GGG</h1>
+    
+
+            </div>
+            <div class="col-12 col-md-12">
+                <x-frontend.topstorycard></x-frontend.topstorycard>
+            </div>
+            <div class="col-12 col-md-4">
+                <x-frontend.top-block-card></x-frontend.top-block-card>
+            </div>
+ <div class="col-12 col-md-4">
+                   <x-frontend.top-block-card></x-frontend.top-block-card>
+            </div>
+             <div class="col-12 col-md-4">
+                            <x-frontend.top-block-card></x-frontend.top-block-card>
             </div>
         </div>
     </div>
