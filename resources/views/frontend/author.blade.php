4ccd09ec6308829b6d02a8ec9c6e74bc6bca5583
Josh Bello <Jb230913@live.com>
2026-09-18 12:31:46 +0100

    add - new page

---
+++ resources/views/frontend/author.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+</div>
+
+@extends('layouts.frontend-default-layout')
+@section('title','Author -')
+@section('content')
+
+@endsection
