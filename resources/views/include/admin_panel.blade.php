3f7eb2c4e2af857ed04907a4d50678e9377e401b
Josh Bello <Jb230913@live.com>
2026-09-19 12:39:03 +0100

    update - added profile nav item

--- resources/views/include/admin_panel.blade.php
+++ resources/views/include/admin_panel.blade.php
@@ -8,20 +8,11 @@
             Admin Panel
         </a>
 
-        <button
-            {{-- type="button"
+        <button {{-- type="button"
             class="btn btn-outline-light sidebar-close"
             id="sidebarClose"
             data-bs-dismiss="offcanvas"
-            aria-label="Close navigation" --}}
-
-            class="btn btn-outline-light sidebar-close"
-    type="button"
-    data-bs-toggle="offcanvas"
-    data-bs-target="#adminSidebar"
-    aria-controls="adminSidebar"
-    aria-label="Toggle navigation"
-        >
+            aria-label="Close navigation" --}} class="btn btn-outline-light sidebar-close" type="button" data-bs-toggle="offcanvas" data-bs-target="#adminSidebar" aria-controls="adminSidebar" aria-label="Toggle navigation">
             &times;
         </button>
 
@@ -32,29 +23,66 @@
 
     <nav class="nav flex-column">
 
-        <a href={{ route('admin.dashboard') }} class="nav-link active">
+        <a href={{ route('admin.dashboard') }} @if(request()->is('admin/dashboard'))
+            class="nav-link active"
+            @else
+            class="nav-link"
+            @endif
+            >
             <span class="me-2"><i class="bi bi-bar-chart-line"></i></span>
             Dashboard
         </a>
 
-        <a href={{ route('admin.post.index') }} class="nav-link">
+        <a href={{ route('admin.post.index') }} @if(request()->is('admin/posts*'))
+            class="nav-link active"
+            @else
+            class="nav-link"
+            @endif
+            >
             <span class="me-2"><i class="bi bi-person-lines-fill"></i></span>
             Posts
         </a>
 
-        <a href={{ route('admin.media.index') }} class="nav-link">
+        <a href={{ route('admin.media.index') }} @if(request()->is('admin/media*'))
+            class="nav-link active"
+            @else
+            class="nav-link"
+            @endif>
             <span class="me-2"><i class="bi bi-collection-fill"></i></span>
             Media
         </a>
 
-        <a href={{ route('admin.category.index') }}  class="nav-link">
+        <a href={{ route('admin.category.index') }}
+            @if(request()->is('admin/category*'))
+            class="nav-link active"
+            @else
+            class="nav-link"
+            @endif
+        >
             <span class="me-2"><i class="bi bi-bookmark"></i></span>
             Categories
         </a>
 
-        <a href={{ route('admin.tags.index') }} class="nav-link">
+        <a href={{ route('admin.tags.index') }}
+             @if(request()->is('admin/tags*'))
+            class="nav-link active"
+            @else
+            class="nav-link"
+            @endif
+        >
             <span class="me-2"><i class="bi bi-tags"></i></span>
             Tags
+        </a>
+
+        <a href={{ route('admin.profile') }}
+             @if(request()->is('admin/profile*'))
+            class="nav-link active"
+            @else
+            class="nav-link"
+            @endif
+        >
+            <span class="me-2"><i class="bi bi-tags"></i></span>
+            Profile
         </a>
 
     </nav>
