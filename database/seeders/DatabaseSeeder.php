24933a8f7c0ed9fa28343edd94c950613676abbe
Josh Bello <Jb230913@live.com>
2026-09-22 14:54:31 +0100

    update - add call() method to databaseSeeder to follow steps for data insertion

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            CategorySeeder::class,
+            TagSeeder::class,
+
+            PostSeeder::class,
+
+            PostSecondaryCategorySeeder::class,
+            PostTagSeeder::class,
+
+            MediaSeeder::class,
+            CommentSeeder::class,
         ]);
     }
 }
