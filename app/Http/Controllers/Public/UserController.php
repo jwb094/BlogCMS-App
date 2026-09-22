475032f076c2e9d294b5428b3548ad15c4fc48e2
Josh Bello <Jb230913@live.com>
2026-09-22 10:17:43 +0100

    update - added logic for signin, store and logout

--- app/Http/Controllers/Public/UserController.php
+++ app/Http/Controllers/Public/UserController.php
@@ -3,17 +3,71 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserFormRequest;
+use App\Http\Requests\UserLoginRequest;
+use App\Services\PublicService;
+use App\Services\UserService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
-    public function  login() {
+    protected PublicService $publicService;
+    protected UserService $userService;
+    public function __construct(
+        PublicService $publicService,
+        UserService $userService
+    ) {
+        $this->publicService = $publicService;
+        $this->userService = $userService;
+    }
+    public function  login()
+    {
 
-    return view('frontend.auth.login');
+        return view('frontend.auth.login');
     }
-    public function signin() {}
-    public function register() {
-           return view('frontend.auth.register');
+    public function signin(UserLoginRequest $request)
+    {
+        $loginCredentials = $request->validated();
+
+        $authenciated = $this->userService->userAuth($loginCredentials);
+
+        if (!$authenciated) {
+            return redirect()
+                ->intended(route('login'))
+                ->with('success', "You have successfully logged in");
+        }
+
+        return  redirect(route('dashboard'))
+            ->with('message', "Login successfully");
     }
-    public function store() {}
+    public function register()
+    {
+        return view('frontend.auth.register');
+    }
+    public function store(UserFormRequest $request)
+    {
+
+        $newUser =  $this->userService->store($request->validated());
+
+        if (!$newUser->id) {
+            return  redirect(route('profile.register'))
+                ->with('status', false)
+                ->with('message', "Registration failed, try again please");;
+        }
+        return  redirect(route('profile.login'))
+            ->with('status', true)
+            ->with('message', "Registration successfully");;
+    }
+
+
+    public function logout()
+    {
+
+        Session::flush();
+        Auth::logout();
+
+        return  redirect(route('login'));
+    }
 }
