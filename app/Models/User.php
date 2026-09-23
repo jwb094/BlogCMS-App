3ac35ec8eb162aa4abb443dc38d8b11de2dc342e
Josh Bello <Jb230913@live.com>
2026-09-23 10:19:01 +0100

    update - additional fields added to $fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password','first_name','last_name'])]
+#[Fillable(['name', 'email', 'password','first_name','last_name','role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
