1340a2e9fee9d4991692a64f272c509bcdedb85c
Josh Bello <Jb230913@live.com>
2026-09-24 20:43:56 +0100

    update - updateUserAccount userPasswordUpdate added

--- app/Services/AdminUserService.php
+++ app/Services/AdminUserService.php
@@ -3,7 +3,52 @@
 
 namespace App\Services;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
-class AdminUserService{
-    
+class AdminUserService
+{
+
+
+    public function updateUserAccount(User $user, array $data): User
+    {
+
+        // $updatedUser = User::findOrFail($updatedUserDataId);
+
+
+        if (isset($data['profile_photo'])) {
+
+            if ($user->profile_photo) {
+                Storage::disk('public')->delete(
+                    $user->profile_photo
+                );
+            }
+
+            $data['profile_photo'] = $data['profile_photo']
+                ->store('profile-photos', 'public');
+        }
+        //dd($data);
+        $user->update($data);
+
+        return $user->refresh();
+        // $updatedUser->update($updatedUserData);
+
+        // return $updatedUser->refresh();
+    }
+
+
+
+    public function userPasswordUpdate(int $updatedUserDataId, array $updatedUserData): User
+    {
+
+        $user = User::where('id', $updatedUserDataId)
+            ->firstOrFail();
+
+        $user->update([
+            'password' => Hash::make($updatedUserData['password']),
+        ]);
+
+        return $user->refresh();
+    }
 }
