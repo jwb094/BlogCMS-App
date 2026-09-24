a790e8082be98956fda3769774ce505a442f5d22
Josh Bello <Jb230913@live.com>
2026-09-24 20:43:11 +0100

    update - linkedin,twitter,instagram,facebook,profile_photo,bio added to $fillable array

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password','first_name','last_name','role'])]
+#[Fillable(['name', 'email', 'password','first_name','last_name','role','linkedin','twitter','instagram','facebook','profile_photo','bio'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable implements MustVerifyEmail
 {
