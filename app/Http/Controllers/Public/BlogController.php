45efed527c6fd341e6b7cb1b27a51288db17bb78
Josh Bello <Jb230913@live.com>
2026-09-24 10:58:39 +0100

    update - change view file name

--- app/Http/Controllers/Public/BlogController.php
+++ app/Http/Controllers/Public/BlogController.php
@@ -13,7 +13,7 @@ class BlogController extends Controller
      */
     public function index()
     {
-        return view('frontend.blog.index');
+        return view('frontend.index');
     }
 
      /**
