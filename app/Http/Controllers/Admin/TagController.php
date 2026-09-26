8af5b7b6445b6075dcb7f3114638b1c6def00576
Josh Bello <Jb230913@live.com>
2026-09-26 14:13:22 +0100

    update - added Logic to action methods

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TagFormRequest;
+use App\Models\Tag;
+use App\Services\BlogService;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
+    //protected BlogService $blogService;
+    public function __construct(
+        protected BlogService $blogService
+    ) {}
+
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('backend.tags.index');
+        $data = $this->blogService->Index($request);
+        return view('backend.tags.index', ['data' => $data]);
     }
 
     /**
@@ -20,15 +30,25 @@ class TagController extends Controller
      */
     public function create()
     {
-        //
+        return view('backend.tags.add');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TagFormRequest $request)
     {
         //
+        $newTag = $this->blogService->newTag($request->validated());
+
+        if (!$newTag->id) {
+            return  redirect(route('admin.tags.create'))
+                ->withInput()
+                ->with('status', false)->with('message', "New Tag didn't save, try again please");;
+        }
+        return  redirect(route('admin.tags.index'))
+            ->with('status', true)
+            ->with('message', "New Tag added ");
     }
 
     /**
@@ -42,24 +62,34 @@ class TagController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Tag $tag)
     {
-        return view('backend.tags.edit');
+
+        return view('backend.tags.edit', [
+            "tag" => $tag
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(TagFormRequest $request, Tag $tag)
     {
         //
+        $this->blogService->updateTag($request->validated(), $tag->id);
+
+        return redirect()->route('admin.tags.index')
+            ->with('success', "Tag was updated");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Tag $tag)
     {
         //
+        $tag->delete();
+        return redirect(route('admin.tags.index'))
+        ->with('Status', "Tag Record was delete");
     }
 }
