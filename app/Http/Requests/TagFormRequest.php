99e3e22bef5fbe4ca289f5272c45459f6452e272
Josh Bello <Jb230913@live.com>
2026-09-26 08:59:56 +0100

    update - changes made from default setup

--- app/Http/Requests/TagFormRequest.php
+++ app/Http/Requests/TagFormRequest.php
@@ -12,7 +12,7 @@ class TagFormRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,10 @@ class TagFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'   => 'required|string|max:255',
+            'description'   => 'nullable|string|max:255',
+            'meta_title' => 'nullable|string|max:255',
+            'meta_description' => 'nullable|string|max:255',
         ];
     }
 }
