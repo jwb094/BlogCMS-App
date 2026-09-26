46265616a0d2ea86b474ca635800f5da5477b6ce
Josh Bello <Jb230913@live.com>
2026-09-26 14:12:57 +0100

    update - added html elements and directive

--- resources/views/backend/tags/add.blade.php
+++ resources/views/backend/tags/add.blade.php
@@ -1,8 +1,51 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
-</div>
 @extends('layouts.backend-layout')
 @section('title','Tags - New Tag')
 @section('content')
 
+<div class="container-fluid">
+    <div class="row">
+        @if ($errors->any())
+        <div class="w-100 d-flex justify-content-center align-items-center mx-auto">
+            <x-form.alert class="alert alert-danger">
+                <ul class="mb-0 p-0">
+                    @foreach ($errors->all() as $error)
+                    <li class="mb-1">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </x-form.alert>
+        </div>
+        @endif
+
+        <!-- Main content -->
+        <div class="col-md-12 ms-sm-auto col-lg-12 px-md-4 my-auto">
+
+            <section class=" my-5 mx-auto">
+                <form action={{ route('admin.tags.store') }} method="POST">
+                    @csrf
+                    <h1 class="h3 mb-3 fw-normal text-center"> New Tag</h1>
+                    <div class="form-check mt-3">
+                        <x-form.label for="tagname" class="form-label"> Name</x-form.label>
+                        <x-form.input type="text" id="tagname" class="form-control" name="name"></x-form.input>
+                    </div>
+                    <div class="form-check mt-3">
+                        <x-form.label for="description" class="form-label">Description</x-form.label>
+                        <x-form.textarea type="text" id="description" class="form-control" name="description"></x-form.textarea>
+                    </div>
+                    <div class="form-check mt-3">
+                        <x-form.label for="meta_title" class="form-label"> Meta title</x-form.label>
+                        <x-form.input type="text" id="meta_title" class="form-control" name="meta_title"></x-form.input>
+                    </div>
+                    <div class="form-check mt-3">
+                        <x-form.label for="meta_description" class="form-label"> Meta Description</x-form.label>
+                        <x-form.textarea type="text" id="meta_description" class="form-control" name="meta_description"></x-form.textarea>
+                    </div>
+
+                    <x-form.button class="btn btn-primary w-100 py-2 mt-2" type="submit">Save</x-form.button>
+
+                </form>
+            </section>
+        </div>
+
+    </div>
+</div>
 @endsection
--- resources/views/backend/tags/index.blade.php
+++ resources/views/backend/tags/index.blade.php
@@ -2,4 +2,56 @@
 @section('title','Tags - Dashboard')
 @section('content')
 
-@endsection
+<div class="container-fluid">
+    <div class="row">
+        <div class="my-4">
+            <a class="col-sm-2 btn btn-primary" href="{{ route('admin.tags.create') }}">New Tag</a>
+        </div>
+        <form action={{ route('admin.tags.index') }} method="GET">
+
+            <div class="row">
+                <div class="col-xs-12 col-md-2 mb-3">
+                    <x-form.input type="text" id="tagname" class="form-control" name="name" placeholder="Friends"></x-form.input>
+                </div>
+                <div class="col-12 col-md-3 mb-3 | form-check">
+                    <button type="submit" class="btn btn-primary mt-6 | text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5">
+                        Search
+                    </button>
+
+                </div>
+                <div class="col-12 col-md-3 mb-3 | form-check">
+                    <a class="btn btn-secondary" href={{ route('admin.tags.index') }}> Reset</a>
+                </div>
+            </div>
+
+        </form>
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr class="table-dark">
+                        <th scope="col">Name</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                 @foreach($data['tags'] as $key => $value)
+                        <tr>
+                            <td>{{ $value->name }}</td>
+                            <td>
+                                <a href={{ route('admin.tags.edit',$value) }} class="btn btn-primary">Update</a>
+                                <form action="{{ route('admin.tags.destroy',$value) }}" method="POST" style="display:inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this Tag?')">Delete</button>
+                                </form>
+                            <td></td>
+                        </tr>
+                        @endforeach
+                </tbody>
+            </table>
+        </div>
+          <div class="d-flex justify-content-center mt-4">
+                {{ $data['tags']->links() }}
+            </div>
+    </div>
+    @endsection
